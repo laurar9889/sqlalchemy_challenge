737837855de3f25d8f9e2278f3e62e0cffde0ae0
laurar9889 <laurar9889@hotmail.com>
2024-05-05 21:53:32 -0400

    Assignment: Design Flask API - in progress -

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
 # Import the dependencies.
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
 
 
 
 #################################################
 # Database Setup
 #################################################
+#Create an engine for the hawaii.sqlite database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
 
-
-# reflect an existing database into a new model
+# reflect an existing database into ORM classes
+Base = automap_base()
+Base.prepare(autoload_with = engine)
 
 # reflect the tables
-
+Base.classes.keys()
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(bin=engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/api/v1.0/hawaii")
+def hawaii():
+    """Return the justice league data as json"""
+    return jsonify()
+
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to the Hawaii weather API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        
+    )
+    
