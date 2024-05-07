3c6b54f39ae0df4d7e349bcd4ab437bb2c6117b2
laurar9889 <laurar9889@hotmail.com>
2024-05-07 19:54:39 -0400

    Assignment: Add list of available api routes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import the dependencies.
 from flask import Flask, jsonify
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
 
 
@@ -8,11 +10,11 @@ from sqlalchemy import create_engine
 # Database Setup
 #################################################
 #Create an engine for the hawaii.sqlite database
-engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into ORM classes
 Base = automap_base()
-Base.prepare(autoload_with = engine)
+Base.prepare(autoload_with=engine)
 
 # reflect the tables
 Base.classes.keys()
@@ -22,7 +24,7 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-session = Session(bin=engine)
+session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -34,17 +36,23 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-@app.route("/api/v1.0/hawaii")
-def hawaii():
-    """Return the justice league data as json"""
-    return jsonify()
-
 @app.route("/")
 def welcome():
+    """Homepage: List all available api routes"""
     return(
-        f"Welcome to the Hawaii weather API!<br/>"
-        f"Available Routes:<br/>"
+        f"Welcome to the Hawaii weather API!. Availabe rountes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        
-    )
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/temperature<br/>"
+        f"/api/v1.0/ave temperature, min and max temperature"
     
+    )
+
+#Draft the code for each page:
+@app.route("/api/v1.0/precipitation<br/>")
+def precipitation():
+     # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Query all precipitation data for the last 12  months
+    prcp_12month = session.query()
